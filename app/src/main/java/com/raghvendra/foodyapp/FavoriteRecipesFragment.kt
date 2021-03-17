9736a800eef4d2a95704610b174989e8a9248291
raghu_123 <raghvendra@nimapinfotech.com>
2021-03-18 00:16:16 +0530

    Added recipes_row_layout.xml and placeholder_row_layout.xml

--- app/src/main/java/com/raghvendra/foodyapp/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/raghvendra/foodyapp/FavoriteRecipesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 
 /**
@@ -24,7 +25,10 @@ class FavoriteRecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+
+         val view =  inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+
+        return view
     }
 
 
